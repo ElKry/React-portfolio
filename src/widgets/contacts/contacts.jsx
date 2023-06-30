91ae816cc3cc643cadfc2e1fc6c195caef17254b
Elizaveta Kriazhkova <elizaveta.kriazhkova@optimax.dev>
2023-06-30 13:51:14 +0400

    added form

--- src/widgets/contacts/contacts.jsx
+++ src/widgets/contacts/contacts.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Element } from "react-scroll";
+import { Form } from "../../features/navigation/form/form";
 
 import "./contacts.css";
 
 export const Contacts = () => {
   return (
     <Element name="contacts" className="contacts">
-      Contacts
+      <Form />
     </Element>
   );
 };
